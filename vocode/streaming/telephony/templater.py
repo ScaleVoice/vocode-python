e393be4f0eac92729f1cc17a675bea3bf63e40d1
Ruslan Rozbeiko <ruslan.rozbeiko@resulmatic.com>
2023-12-01 11:03:43 +0100

    ENH: conditional ssl

--- vocode/streaming/telephony/templater.py
+++ vocode/streaming/telephony/templater.py
@@ -1,6 +1,7 @@
 import os
-from jinja2 import Environment, FileSystemLoader
+
 from fastapi import Response
+from jinja2 import Environment, FileSystemLoader
 
 
 class Templater:
@@ -14,7 +15,9 @@ class Templater:
         return template.render(**kwargs)
 
     def get_connection_twiml(self, call_id: str, base_url: str):
+        use_ssl = os.getenv("USE_SSL", "true").lower() == "true"
+        protocol = "wss" if use_ssl else "ws"
         return Response(
-            self.render_template("connect_call.xml", base_url=base_url, id=call_id),
+            self.render_template("connect_call.xml", base_url=base_url, id=call_id, protocol=protocol),
             media_type="application/xml",
         )
